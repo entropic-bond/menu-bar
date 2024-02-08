879252ff8cad8936800efb483071631ed4c8c874
jseto <jsetojseto@gmail.com>
2024-02-08 17:17:41 +0700

    feat: esm and cjs

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vitest/config'
+import dts from 'vite-plugin-dts'
+
 
 export default defineConfig({
   test: {
@@ -7,4 +10,15 @@ export default defineConfig({
 		setupFiles: './add-ons/setup-tests.ts',
 		exclude: ['**/node_modules', '**/dist', '.idea', '.git', '.cache','**/lib', '**/out'],
 	},
+	build: {
+		lib: {
+			entry: resolve( __dirname, 'src/index.ts' ),
+			name: 'entropic-bond-menu-bar',
+			fileName: 'entropic-bond-menu-bar'
+		},
+		"outDir": "lib",
+	},
+	plugins: [
+		dts()
+	]
 })
